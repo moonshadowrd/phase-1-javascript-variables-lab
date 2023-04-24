7b707fae0b79be560bf7d510eda0a78070a7d251
jose cabreja <cabreja@yahoo.com>
2023-04-23 23:43:33 -0400

    adds myfile.md

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const companyName = "Scuber";
-
-
+const companyName = 'Scuber';
 
 let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith'
-it('is set as Scuber', function () {
-    expect(mostProfitableNeighborhood).to.equal('Chelsea');
-  });
 
-
-    expect(companyCeo).to.equal('Susan Smith');
-
-
+let companyCeo = 'Susan Smith';
